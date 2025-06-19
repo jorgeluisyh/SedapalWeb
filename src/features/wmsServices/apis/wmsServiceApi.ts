7e1d5927cfc3b9b302927a2e6807b128982431a5
Jorge Luis YH <32646673+jorgeluisyh@users.noreply.github.com>
2025-06-19 12:29:27 -0500

    servicios wms

--- src/features/wmsServices/apis/wmsServiceApi.ts
+++ src/features/wmsServices/apis/wmsServiceApi.ts
@@ -3,22 +3,22 @@ import type { WmsService } from "../types/wmsServiceType";
 
 
 export const getWmsServices = async (): Promise<WmsService[]> => {
-    const response = await axiosInstance.get("/api/Servicios/serviciosmapa");
+    const response = await axiosInstance.get("/api/Servicios/servicioswms");
     return response.data;
 }
 
 export const postWmsService = async (data: WmsService) => {
-    const response = await axiosInstance.post("/api/Servicios/serviciosmapa", data);
+    const response = await axiosInstance.post("/api/Servicios/servicioswms", data);
     return response.data;
 }
 
 export const updateWmsService = async (id: number, data: WmsService) => {
-    const response = await axiosInstance.put(`/api/Servicios/serviciosmapa/${id}`, data);
+    const response = await axiosInstance.put(`/api/Servicios/servicioswms/${id}`, data);
     return response.data;
 }
 
 export const deleteWmsService = async (id: number) => {
-    const response = await axiosInstance.delete(`/api/Servicios/serviciosmapa/${id}`);
+    const response = await axiosInstance.delete(`/api/Servicios/servicioswms/${id}`);
     return response.data;
 }
 
